5f4c09644940d98ba0ef71c3df6a8b11f8c75451
NSIX06 <felipebugalho2016@gmail.com>
2023-09-20 10:38:02 -0300

    final

--- js/tema.js
+++ js/tema.js
@@ -1,5 +1,17 @@
 function mudarTema()
 {
-    document.body.classList.toggle("escuro");
+   var  d = document.body.classList.toggle("escuro");
+    var l = document.getElementById("btn");
+
+    if( d == true ) 
+    {
+        l.innerHTML = "light_mode";
+    }
+    else
+    {
+
+        l.innerHTML = "dark_mode";
+    }
+
 
 }
